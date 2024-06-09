c7efa9be528bea04578673c80a11e0f04d6dec85
Mark Nygaard <mark.nygaard@hotmail.com>
2024-06-09 14:01:03 +0200

    added tsup to simplify imports

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,4 +1,4 @@
-import "@repo/ui/globals.css";
+import "@repo/ui";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,4 @@
+import "./globals.css";
+
+export * from "./components/ui/button";
+export * from "./lib/utils";
